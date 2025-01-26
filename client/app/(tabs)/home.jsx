9eb73ac4c98a8a00f11b0c07e4d8622e6c086726
ramu <cs24m110@iittp.ac.in>
2025-01-26 14:37:38 +0530

    created the dashboard file for design and added few image assets.

--- client/app/(tabs)/home.jsx
+++ client/app/(tabs)/home.jsx
@@ -1,11 +1,12 @@
 import {View, Text} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
+import DashBoardCard from "../../components/DashBoardCard";
 
 const Home = () => {
     return (
-        <SafeAreaView className={"flex items-center justify-center"}>
-            <Text>Home</Text>
+        <SafeAreaView className={"flex items-center justify-center bg-black-100 h-full fixed"}>
+            <DashBoardCard bottomLeft={10} bottomRight={10} topLeft={10} topRight={10} />
         </SafeAreaView>
     )
 }
